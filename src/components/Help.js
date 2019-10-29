eee296aa80929efab5aa94fb7a235e9f739529cb
Alex1812r <alex1812r@gmail.com>
2019-10-29 15:02:25 -0400

    closed #6

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Tabs from './Tabs/Tabs'
 
 export default function Help() {
     // Declare a new state variable, which we'll call "isOpen"
@@ -9,13 +10,21 @@ export default function Help() {
             <button onClick={() => setIsOpen(!isOpen)}>
                 {isOpen ? 'Close' : 'Open'} Instructions
             </button>
-            {isOpen && <div>
-                <p className='instructions'>Hold down 'g' and click anywhere to start creating a Goal</p>
-                <p className='instructions'>Click on a node to select it</p>
-                <p className='instructions'>With a node selected, hold down 'g' and click anywhere to create a connected (child) Goal</p>
-                <p className='instructions'>Press 'Esc' to close the Goal creator and deselect Goals</p>
-                <p className='instructions'>Press 'Delete' to archive a Goal</p>
-            </div>}
+            {isOpen &&
+                <div id="guide-book">
+                    <h4>Guide Book</h4>
+                    <Tabs 
+                    tabs={[ {title: 'Getting Started'}, {title: "How To's"}, {title: 'Shortcuts'}, {title: 'FAQ'}]} >
+                        <div>
+                            <p className='instructions'>Hold down 'g' and click anywhere to start creating a Goal</p>
+                            <p className='instructions'>Click on a node to select it</p>
+                            <p className='instructions'>With a node selected, hold down 'g' and click anywhere to create a connected (child) Goal</p>
+                            <p className='instructions'>Press 'Esc' to close the Goal creator and deselect Goals</p>
+                            <p className='instructions'>Press 'Delete' to archive a Goal</p>
+                        </div>
+                    </Tabs>
+                </div>
+            }
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   }
