d3cea87da08a42eb005cbd920ac5e099519259ea
Alex1812r <alex1812r@gmail.com>
2019-11-04 17:58:37 -0400

    #10 almost done

--- src/components/GuideBook/GuideBook.js
+++ src/components/GuideBook/GuideBook.js
@@ -1,96 +1,65 @@
 import React from 'react'
 import './GuideBook.css'
-import Tabs from '../Tabs/Tabs'
+import Tabs from './Tabs/Tabs'
 import GettingStarted from './Sections/GettingStarted'
+import HowTos from './Sections/HowTos'
 
 export default class GuideBook extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      tabSelected: null,
+      sectionSelected: null,
+      tabs: [ {title: 'Getting Started'}, {title: "How To's"}, {title: 'Shortcuts'}, {title: 'FAQ'}],
+    }
+
+    this.handleSelectTab = this.handleSelectTab.bind(this)
+    this.handleSelectSection = this.handleSelectSection.bind(this)
+    this.handleGoBack = this.handleGoBack.bind(this)
+  }
+
+  handleSelectTab(tab){
+    this.setState({ tabSelected: tab, sectionSelected: null })
+  }
+
+  handleSelectSection(section){
+    this.state.tabs.map((tab,index)=>{
+      if(tab.title === section.tab){
+        this.setState({ tabSelected: index })
+      }
+      this.setState({
+        sectionSelected: section.url
+      })
+    })
+  }
+
+  handleGoBack(){
+    this.setState({ sectionSelected: null })
+  }
+
   render(){
     return(
       <div id="guide-book">
         <h2 className="guidebook-title">Guide Book</h2>
         <Tabs
-          tabs={[ {title: 'Getting Started'}, {title: "How To's"}, {title: 'Shortcuts'}, {title: 'FAQ'}]}
+          tabs={this.state.tabs}
+          selected={this.state.tabSelected}
+          toSelectTab={this.handleSelectTab}
         >
-          <GettingStarted />
+            <GettingStarted
+              sectionSelected={this.state.sectionSelected}
+              selectSection={this.handleSelectSection}
+              goBack={this.handleGoBack}
+            />
+
+            <HowTos 
+              sectionSelected={this.state.sectionSelected}
+              selectSection={this.handleSelectSection}
+              goBack={this.handleGoBack}
+            />
         </Tabs>
       </div>
-    )
-  }
-}
-
-export function NavItemsGroup(props) {
-  return(
-    <nav>
-      { 
-        props.items &&
-        props.items.map((ni,i)=>(
-          <NavItem
-            key={i}
-            id={i}
-            title={ni.title}
-            icon={ni.icon}
-            url={ni.url}
-            submenu={ni.submenu}
-            className={ni.className}
-            selectSection={props.selectSection}
-          />
-        ))
-      }
-    </nav> 
-  )
-}
-
-class NavItem extends React.Component{
-  
-  constructor(props){
-    super(props)
-    this.state ={
-      expand: false,
-    }
-    this.handleExpand = this.handleExpand.bind(this)
-  }
-  
-
-  handleExpand(){
-    this.setState({
-      expand: this.state.expand ? false : true
-    })
-  }
-
-  render(){
-    const expand = this.state.expand ? 'active' : ''
-    return(
-      <section>
-      {
-        this.props.submenu 
-        ?
-          <>
-          <button 
-            className={`nav-item ${this.props.className || ''}`}
-            onClick={this.handleExpand}>
-            {this.props.title}
-            </button>
-          <div className={`sidebar-submenu ${expand}`}>
-            <ul>
-            {
-              this.props.submenu.map((ni,i)=>(
-                <li key={i}>
-                  {/* <NavLink to={ni.url}><span>-</span> {ni.title}</NavLink> */}
-                  <button type="button" onClick={()=>{this.props.selectSection(ni.url)}}>{ni.title}</button>
-                </li>
-              ))
-            }
-            </ul>     
-          </div>
-          </>
-        :
-        // <NavLink className={`nav-item ${this.props.className || ''}`} to={this.props.url ? this.props.url : '/' }>
-        //   {this.props.icon && <img src={this.props.icon} alt=""/> }
-        //   {this.props.title && this.props.title}
-        // </NavLink>
-        <>{ this.props.title} </>
-      }
-      </section>
     )
   }
 }
--- src/components/GuideBook/Sections/GettingStarted.js
+++ src/components/GuideBook/Sections/GettingStarted.js
@@ -1,72 +1,28 @@
 import React from 'react'
-import { NavItemsGroup } from '../GuideBook'
+import { NavItemsGroup } from '../NavItems/NavItems'
 
-export default class GettingStarted extends React.Component{
-
-  constructor(props){
-    super(props)
-    this.state = { selectd: null }
-
-    this.handleSelectSection = this.handleSelectSection.bind(this)
-    this.handleGoBack = this.handleGoBack.bind(this)
-  }
-
-  handleSelectSection(section){
-    this.setState({
-      selectd: section
-    })
-  }
-
-  handleGoBack(url){
-    this.setState({
-      selectd: url
-    })
-  }
-
-  render(){
-    const navItems = [
-      {
-        title: 'Basic',
-        submenu: [
-            { title: 'Create a Card', url: 'create_card'},
-            { title: 'Add a child card to an existing card', url: 'add_child_card'},
-            { title: 'Change card title', url: 'change_card_title'},
-            { title: 'Associate members with a card', url: 'associate_members_with_card' },
-            { title: 'change card status', url: 'change_card_status' },
-            { title: 'select multiple cards', url: 'select_multiple_cards' },
-        ]
-      }
+export default function GettingStarted(props){
+  const navItems =[
+  {
+    title: 'Basic',
+    submenu: [
+        { title: 'Create a Card', url: 'create_card', tab: "How To's"},
+        { title: 'Add a child card to an existing card', url: 'add_child_card', tab: "How To's"},
+        { title: 'Change card title', url: 'change_card_title', tab: "How To's"},
+        { title: 'Associate members with a card', url: 'associate_members_with_card', tab: "How To's" },
+        { title: 'Change card status', url: 'change_card_status', tab: "How To's" },
+        { title: 'select multiple cards', url: 'select_multiple_cards',tab: "How To's" },
     ]
-
-    switch(this.state.selectd){
-      case 'create_card': 
-        return <Test goBack={this.handleGoBack} />
-        break;
-      case 'add_child_card':
-        return <></> 
-      default:
-        return(
-          <div id="getting-started">
-            {          
-              <>
-              <h3>Welcome to acorn!</h3>
-                <NavItemsGroup 
-                  items={navItems}
-                  selectSection={ this.handleSelectSection }
-                />
-              </>
-
-            }
-
-            {/* <p className='instructions'>Hold down 'g' and click anywhere to start creating a Goal</p>
-            <p className='instructions'>Click on a node to select it</p>
-            <p className='instructions'>With a node selected, hold down 'g' and click anywhere to create a connected (child) Goal</p>
-            <p className='instructions'>Press 'Esc' to close the Goal creator and deselect Goals</p>
-            <p className='instructions'>Press 'Delete' to archive a Goal</p> */}
-          </div>  
-        ) 
-    }
-  }
+    },
+  ]
+  
+  return (
+    <NavItemsGroup
+      items={navItems}
+      selectSection={props.selectSection} 
+    />
+  )
+     
 }
 
 const Test = (props) => (
