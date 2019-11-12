027d23c09ed7f7de2edbf1a7836f87bb40a77293
hector vasquez <hectorvasquez05@gmail.com>
2019-11-12 12:44:56 -0400

    drawSelectBox

---
+++ src/drawing/drawSelectBox.js
@@ -0,0 +1,18 @@
+
+  function SelectBox(ctx, x, y, w, h,) {
+    ctx.beginPath()
+        
+        ctx.moveTo(x,y)
+        ctx.lineTo(x+w,y)
+        ctx.lineTo(x+w,y+h)
+        ctx.lineTo(x,y+h)
+        ctx.lineTo(x,y)
+
+        ctx.closePath()
+        ctx.stroke()
+  }
+  
+  // render a goal card
+  export default function render(  {x,y}, w,h , ctx) {
+    SelectBox(ctx,x,y,w,h)
+  }
