6c06ad6801c6e83e076ae2dbacab918af1532384
Connor Turland <connorturland@gmail.com>
2019-11-21 19:51:16 -0500

    fix img assets for electron

--- src/components/EmptyState/EmptyState.js
+++ src/components/EmptyState/EmptyState.js
@@ -5,7 +5,7 @@ import React from 'react'
 function EmptyState() {
     return <div className='empty_state_wrapper'>
         <div className='empty_state'>
-            <img src='/img/squirrel_illustration_empty.png' />
+            <img src='img/squirrel_illustration_empty.png' />
             <h1>welcome to acorn</h1>
             <h4>Hold G and left click anywhere on canvas to create your first goal card.</h4>
             <h4>Happy squirreling :)</h4>
