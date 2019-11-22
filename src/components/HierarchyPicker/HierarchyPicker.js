94a8dbba88b857684795a5127da32e4b39b3e1ba
Alex1812r <alex1812r@gmail.com>
2019-11-22 18:14:54 -0400

    position card icon hierarchy done in vertical bar

--- src/components/HierarchyPicker/HierarchyPicker.js
+++ src/components/HierarchyPicker/HierarchyPicker.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import './HierarchyPicker.css'
 import Icon from '../Icon'
+import { connect } from 'react-redux'
+import LayoutFomula from '../../drawing/layoutFormula'
 
-export default function HierarchyPicker({ selectedStatus, statusClicked, onClose }) {
-
+function HierarchyPicker(props) {
+    
     const hierarchies = [
         {
             name: 'Leaf',
@@ -26,51 +28,69 @@ export default function HierarchyPicker({ selectedStatus, statusClicked, onClose
             icon: 'root.png',
             description: 'primary goal'
         },
+        {
+            name: 'No Hierarchy',
+        }
     ]
 
-
-    const [hierarchySelected, setHierarchySelected ] = useState(null)
+    const allCoordenates = LayoutFomula(props.screensize, props.goals, props.edges)
+    let cardSelected
+    let hierarchyIcons
+    if(props.hashSelections.length){
+        cardSelected = props.hashSelections.map(address => allCoordenates[address])
+    }else{
+        cardSelected = allCoordenates[props.hashSelections.goalAddress]
+        const hierarchyDefault = hierarchies.filter( f => f.name === props.selectedHierarchy )
+        hierarchyIcons = hierarchyDefault[0].icon
+    }
     return (
-      <div className='hierarchies_picker vertical_action_overlay'>
-          <span className="hiararchy-card-icon">
-              {
-                  hierarchySelected && 
-                  <Icon name={hierarchySelected} />
-              }
-          </span>
-          <div className="hierarchy_picker_header">
-              <span className="hierarchies_picker_title">Hierarchy</span>
-              <Icon className='vertical_action_close' name='x_a3a3a3.svg' size='small-close' onClick={() => onClose()} />
-          </div>
-          <div className="hierarchy_content_wrapper">
-            {hierarchies.map((hierarchy,index) => (
-                <HierarchyOption
-                    key={index}
-                    name={hierarchy.name}
-                    icon={hierarchy.icon}
-                    description={hierarchy.description}
-                    onClick={ hierarchy => { setHierarchySelected( hierarchy ) }}
-                />
-            ))}
-    
-            <p className="gray-text">
-                Not sure how to set the hierarchy for this card? Read more on our <a className="gray-text" href="#">Guidebook.</a>
-            </p>
-          </div>
-      </div>
+        <div className='hierarchies_picker vertical_action_overlay'>
+            {    
+                cardSelected.length ? 
+                  cardSelected.map((card, index) => (
+                    <span key={index} style={{position: `fixed`, top: `${card.y - 30}px`, left: `${card.x - 28}px`}}>
+                        hola puto
+                    </span>
+                  ))
+                    :
+                    <span style={{position: `fixed`, top: `${cardSelected.y - 30}px`, left: `${cardSelected.x - 28}px`}}>
+                       {
+                           <Icon name={hierarchyIcons} />
+                       }
+                    </span>
+            }
+            <div className="hierarchy_picker_header">
+                <span className="hierarchies_picker_title">Hierarchy</span>
+                <Icon className='vertical_action_close' name='x_a3a3a3.svg' size='small-close' onClick={() => props.onClose()} />
+            </div>
+            <div className="hierarchy_content_wrapper">
+                {hierarchies.map((hierarchy,index) => (
+                    <HierarchyOption
+                        key={index}
+                        name={hierarchy.name}
+                        icon={hierarchy.icon}
+                        description={hierarchy.description}
+                        selected={hierarchy.name === props.selectedHierarchy}
+                        onClick={ hierarchy => { props.hierarchyClicked( hierarchy ) }}
+                    />
+                ))}
+        
+                <p className="gray-text">
+                    Not sure how to set the hierarchy for this card? Read more on our <a className="gray-text" href="#">Guidebook.</a>
+                </p>
+            </div>
+        </div>
     )
 }
 
 HierarchyPicker.propTypes = {
-    selectedHierarchy: PropTypes.string.isRequired,
     hierarchyClicked: PropTypes.func.isRequired
 }
 
 
 function HierarchyOption({selected, onClick, name, icon, description}){
-
     return(
-        <div className="hierarchy_option" onClick={()=>{ onClick(icon) }}>
+        <div className={`hierarchy_option ${ selected ? 'active' : '' }`} onClick={()=>{ onClick(name) }}>
             <div className="hierarchy_option_icon_container">
                 {
                     icon && <img src={`img/${icon}`} />
@@ -78,8 +98,28 @@ function HierarchyOption({selected, onClick, name, icon, description}){
             </div>
             <div className="hierarchy_option_content">
                 <span>{name}</span>
-                <span><small className="gray-text">{description}</small></span>
+                {description &&  <span><small className="gray-text">{description}</small></span>}
             </div>
+            <span>
+                { selected && <img src="img/check-mark-line.svg" alt="check" width="50%"/> }
+            </span>
         </div>
     )    
 }
+
+
+function mapDispatchToProps(dispatch) {
+    return { }
+  }
+  
+  function mapStateToProps(state) {
+    const goalAddress = { goalAddress: state.ui.goalForm.editAddress } 
+    return {
+      hashSelections: goalAddress.goalAddress ? goalAddress : state.ui.selection.selectedGoals ,
+      screensize: state.ui.screensize.width,
+      goals: state.goals,
+      edges: state.edges,
+    }
+  }
+
+export default connect(mapStateToProps,mapDispatchToProps)(HierarchyPicker)
