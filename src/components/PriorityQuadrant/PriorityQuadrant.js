d5c0ade91c2031f4e1a67210dd41b7707e82b423
Pegah <47722137+pegahv@users.noreply.github.com>
2019-11-28 12:59:40 -0500

    changes/updated on priority view and priority quadrants .js and .css

--- src/components/PriorityQuadrant/PriorityQuadrant.js
+++ src/components/PriorityQuadrant/PriorityQuadrant.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import './PriorityQuadrant.css'
 import PriorityGoal from '../PriorityGoal/PriorityGoal'
-import {
-  NavLink
-} from 'react-router-dom'
 
-import Avatar from '../Avatar/Avatar'
-import Icon from '../Icon'
 
 
 function PriorityQuadrant({ title, titleClassname, goals }) {
@@ -14,29 +9,10 @@ function PriorityQuadrant({ title, titleClassname, goals }) {
     <div className={`priority-quadrant-title ${titleClassname}`}>
       {title}
     </div>
-    <div>
-      <div className="priority-quadrant-goals">
-        <div className="priority-quadrant-goal-item">
-          <div className="priority-quadrant-goal-title">
-            <Icon name='leaf_incomplete.svg' size='small' />
-            {goals.map((goal) => {
-              return <PriorityGoal key={goal.content} goal={goal} />
-            })}
-          </div>
-          <div className="priority-quadrant-goal-info">
-            <Icon name='calendar_898989.svg' size='very-small' />
-            <span>Aug 14 - Aug 20</span>
-            <Icon name='profile.png' size='small' />
-            <Avatar />
-          </div>
-          <div className="priority-quadrant-goal-right-menu">
-            <div className="priority-quadrant-goal-view-mode-icons">
-              <NavLink to="/board/map"><Icon name="map_aaaaa.svg" size="view-mode-small" /></NavLink>
-              <Icon name="timeline_aaaaa.svg" size="view-mode-small" />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="priority-quadrant-goals">
+      {goals.map((goal) => {
+        return <PriorityGoal key={goal.content} goal={goal} />
+      })}
     </div>
   </div>
 }
