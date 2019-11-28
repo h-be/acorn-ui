5f9e174e0832298dbe4c33fce76eb9faef0ac5b0
hector vasquez <hectorvasquez05@gmail.com>
2019-11-28 09:31:05 -0400

    add line break

--- src/components/ListExport/ListExport.js
+++ src/components/ListExport/ListExport.js
@@ -1,58 +1,59 @@
-import React from 'react'
+import React from "react";
 
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
-const ListExport = (props) => {
-    return (
-      <a href={url(props.type,props.data)} download={props.download}>{props.title}</a>
-    )
-  }
+const ListExport = props => {
+  return (
+    <a href={url(props.type, props.data)} download={props.download}>
+      {props.title}
+    </a>
+  );
+};
 
 function mapDispatchToProps(dispatch) {
-    return {
-
-    }
-  }
-function url(type,data){
-  let blob ={}
-  if(type==="csv"){
-     const csvRows=[];
-     const agents= Object.keys(data.agents)
-     const goals= Object.keys(data.goals)
-     const edges= Object.keys(data.edges)
-     const goalMembers= Object.keys(data.goalMembers)
-     const loop=(heardes,data)=>{
-     const csvRows=[];
-       
-      csvRows.push(Object.keys(data[heardes[0]]).join(","))
-      for(const index in heardes){
-         csvRows.push(Object.values(data[heardes[index]]))
-      }
-      return csvRows.join("\n")
-     }
-     if(agents.length>0)csvRows.push(loop(agents,data.agents))
-     if(goals.length>0)csvRows.push(loop(goals,data.goals))
-     if(edges.length>0)csvRows.push(loop(edges,data.edges))
-     if(goalMembers.length>0)csvRows.push(loop(goalMembers,data.goalMembers))
-
-     
-     blob = new Blob([csvRows.join("\n")],{type:"text/csv"})
-  }else{
-    blob = new Blob([JSON.stringify(data,null,2)],{type:""})
-   
-  }
-  const url =window.URL.createObjectURL(blob)
-  return url
+  return {};
 }
-  function mapStateToProps(state) {
-      
-    return {
-      data:{agents:state.agents,
-            goals:state.goals,
-            edges:state.edges,
-            goalMembers:state.goalMembers}
-      
-    }
+function url(type, data) {
+  let blob = {};
+  if (type === "csv") {
+    const csvRows = [];
+    const agents = Object.keys(data.agents);
+    const goals = Object.keys(data.goals);
+    const edges = Object.keys(data.edges);
+    const goalMembers = Object.keys(data.goalMembers);
+    const loop = (heardes, data) => {
+      const csvRows = [];
+
+      csvRows.push(Object.keys(data[heardes[0]]).join(","));
+      for (const index in heardes) {
+        csvRows.push(Object.values(data[heardes[index]]));
+      }
+      return csvRows.join("\n");
+    };
+    if (agents.length > 0) csvRows.push(loop(agents, data.agents));
+    if (goals.length > 0) csvRows.push("\n" + loop(goals, data.goals));
+    if (edges.length > 0) csvRows.push("\n" + loop(edges, data.edges));
+    if (goalMembers.length > 0)
+      csvRows.push("\n" + loop(goalMembers, data.goalMembers));
+
+    blob = new Blob([csvRows.join("\n")], {
+      type: "text/csv"
+    });
+  } else {
+    blob = new Blob([JSON.stringify(data, null, 2)], { type: "" });
   }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(ListExport)
+  const url = window.URL.createObjectURL(blob);
+  return url;
+}
+function mapStateToProps(state) {
+  return {
+    data: {
+      agents: state.agents,
+      goals: state.goals,
+      edges: state.edges,
+      goalMembers: state.goalMembers
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListExport);
