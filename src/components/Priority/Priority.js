7993c331f77f30bb00f63b65663dc8aed05ad1b6
hector vasquez <hectorvasquez05@gmail.com>
2019-11-29 17:52:45 -0400

    almost finished

--- src/components/Priority/Priority.js
+++ src/components/Priority/Priority.js
@@ -1,116 +1,188 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import './Priority.css'
-import Icon from '../Icon'
-import Button from '../Button/Button'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "./Priority.css";
+import Icon from "../Icon";
+import Button from "../Button/Button";
+import { connect } from "react-redux";
 
-import Slider from '@material-ui/core/Slider'
-import { makeStyles } from '@material-ui/core/styles';
-
+import Slider from "@material-ui/core/Slider";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  addVoteOfGoal,
+  fetchGoalVotes,
+  archiveVoteOfGoal,
+  updateGoalVote
+} from "../../goal-votes/actions";
 const useStyles = makeStyles({
   root: {
-    color: '#6600FF',
+    color: "#6600FF",
     height: 4,
     borderRadius: 10,
     border: 0,
-    padding: '10px 0',
-    margin: '10px 20px 10px 20px',
-    width: '150px',
+    padding: "10px 0",
+    margin: "10px 20px 10px 20px",
+    width: "150px"
   },
   label: {
-    textTransform: 'capitalize',
+    textTransform: "capitalize"
   },
   rail: {
     height: 4,
-    borderRadius: 10,
+    borderRadius: 10
   },
   mark: {
     height: 4,
     width: 4,
     borderRadius: 4,
-    margin: '0 0 0 -2px',
+    margin: "0 0 0 -2px"
   },
   markLabel: {
-    top: '-14px',
-    position: 'absolute',
-    fontSize: '12px',
+    top: "-14px",
+    position: "absolute",
+    fontSize: "12px",
     fontFamily: '"rennerbook", "Helvetica", "Arial", "sans-serif"',
-    lineHeight: '1',
+    lineHeight: "1"
   },
   offset: {
-    fontFamily: '"rennerbook", "Helvetica", "Arial", "sans-serif"',
+    fontFamily: '"rennerbook", "Helvetica", "Arial", "sans-serif"'
   },
   track: {
     height: 4,
-    borderRadius: 10,
-  },
-})
+    borderRadius: 10
+  }
+});
 
 const marksWithLabels = [
   {
     value: 0,
-    label: 'low',
+    label: "low"
   },
   {
     value: 25,
-    label: '',
+    label: ""
   },
   {
     value: 50,
-    label: 'medium',
+    label: "medium"
   },
   {
     value: 75,
-    label: '',
+    label: ""
   },
   {
     value: 100,
-    label: 'high',
-  },
-]
+    label: "high"
+  }
+];
 
 const marksWithoutLabels = [
   {
     value: 0,
-    label: '',
+    label: ""
   },
   {
     value: 25,
-    label: '',
+    label: ""
   },
   {
     value: 50,
-    label: '',
+    label: ""
   },
   {
     value: 75,
-    label: '',
+    label: ""
   },
   {
     value: 100,
-    label: '',
-  },
-]
+    label: ""
+  }
+];
 
 function valuetext(value) {
   return `${value}%`;
 }
-
-function Priority({ goalAddress, onClose }) {
-
-  const classes = useStyles()
+function Priority({
+  goalAddress,
+  onClose,
+  createGoalVote,
+  whoami,
+  fetchGoalVotes,
+  updateGoalVote
+}) {
+  const classes = useStyles();
 
   const priorityItemVars = [
-    { "priorityIcon": "urgency.svg", "priorityItemTitle": "Urgency" },
-    { "priorityIcon": "importance.svg", "priorityItemTitle": "Importance" },
-    { "priorityIcon": "impact.svg", "priorityItemTitle": "Impact" },
-    { "priorityIcon": "effort.png", "priorityItemTitle": "Effort" },
-  ]
-  const priorityItems = priorityItemVars.map((priorityItem, index) =>
-    <div className="priority_item">
-      <Icon className='priority_item_icon' name={priorityItem.priorityIcon} size='small' />
-      <div className="priority_item_title">{priorityItem.priorityItemTitle}</div>
+    { priorityIcon: "urgency.svg", priorityItemTitle: "Urgency" },
+    { priorityIcon: "importance.svg", priorityItemTitle: "Importance" },
+    { priorityIcon: "impact.svg", priorityItemTitle: "Impact" },
+    { priorityIcon: "effort.png", priorityItemTitle: "Effort" }
+  ];
+  const [value, setValue] = useState({
+    Urgency: 50,
+    Importance: 50,
+    Impact: 50,
+    Effort: 50
+  });
+  const hadleOnChange = e => {
+    if (
+      value[e.target.parentElement.parentElement.children[1].textContent] !==
+      e.target.children[0].children[0].children[0].textContent
+    ) {
+      console.log(
+        "object",
+        e.target.children[0].children[0].children[0].textContent
+      );
+      setValue({
+        ...value,
+        [e.target.parentElement.parentElement.children[1].textContent]:
+          e.target.children[0].children[0].children[0].textContent
+      });
+    }
+  };
+  useEffect(() => {
+    console.log("state", value);
+    let goal_vote = {
+      goal_address: goalAddress,
+      urgency: parseInt(value["Urgency"]) / 100 || 0,
+      importance: parseInt(value["Importance"]) / 100 || 0,
+      impact: value["Impact"] / 100,
+      effort: parseInt(value["Effort"]) / 100 || 0,
+      agent_address: whoami.entry.address,
+      unix_timestamp: Date.now()
+    };
+    fetchGoalVotes().then(value => {
+      const votes = value.filter(v => {
+        return v.entry.goal_address === goalAddress;
+      });
+      const vote = votes.find(v => {
+        return v.entry.agent_address === whoami.entry.address;
+      });
+      console.log("valu", votes.length, vote);
+      if (votes.length > 0 && vote !== undefined) {
+        console.log("update", goal_vote);
+        updateGoalVote({ goal_vote }, vote.address).then(value => {
+          console.log("update", value);
+        });
+      } else {
+        createGoalVote({ goal_vote }).then(value => {
+          console.log("create", value);
+        });
+      }
+    });
+  }, [value]);
+  const priorityItems = priorityItemVars.map((priorityItem, index) => (
+    <div key={index} className="priority_item">
+      <Icon
+        className="priority_item_icon"
+        name={priorityItem.priorityIcon}
+        size="small"
+      />
+      <div key={index} className="priority_item_title">
+        {priorityItem.priorityItemTitle}
+      </div>
       <Slider
+        key={index}
+        onChange={hadleOnChange}
         defaultValue={50}
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider-custom"
@@ -124,60 +196,116 @@ function Priority({ goalAddress, onClose }) {
           mark: classes.mark,
           markLabel: classes.markLabel,
           offset: classes.offset,
-          track: classes.track,
+          track: classes.track
         }}
       />
-    </div>);
+    </div>
+  ));
 
-  const [openMyVote, setOpenMyVote] = useState(false)
+  const [openMyVote, setOpenMyVote] = useState(false);
 
   // aggregated_priority_title
   // my_vote_title
-  const priorityTabClassname = 'priority_tab'
-  let aggClassName = priorityTabClassname
-  let myVoteClassName = priorityTabClassname
+  const priorityTabClassname = "priority_tab";
+  let aggClassName = priorityTabClassname;
+  let myVoteClassName = priorityTabClassname;
   if (openMyVote) {
-    myVoteClassName += ' active'
+    myVoteClassName += " active";
   } else {
-    aggClassName += ' active'
+    aggClassName += " active";
   }
 
   return (
-    <div className='priority_wrapper vertical_action_overlay'>
-      <Icon className='vertical_action_close' name='x_a3a3a3.svg' size='small-close' onClick={() => onClose()} />
+    <div className="priority_wrapper vertical_action_overlay">
+      <Icon
+        className="vertical_action_close"
+        name="x_a3a3a3.svg"
+        size="small-close"
+        onClick={() => onClose()}
+      />
       <div className="popup_title">priority</div>
       <div className="priority_tabs">
-        <div className={aggClassName} onClick={() => setOpenMyVote(false)}>Aggregated Priority</div>
-        <div className={myVoteClassName} onClick={() => setOpenMyVote(true)}>My Vote</div>
+        <div className={aggClassName} onClick={() => setOpenMyVote(false)}>
+          Aggregated Priority
+        </div>
+        <div className={myVoteClassName} onClick={() => setOpenMyVote(true)}>
+          My Vote
+        </div>
       </div>
       {/* TODO : Ensure aggregated priority items are display ONLY */}
-      {!openMyVote && <div className="aggregated_priority">
-        <div className="aggregated_priority_inputs">Based on 5 inputs</div>
-        {priorityItems}
-        <div className="priority_wrapper_button">
-          <Button size="small" color="purple" text="Weigh In" onClick={() => setOpenMyVote(true)} />
+      {!openMyVote && (
+        <div className="aggregated_priority">
+          <div className="aggregated_priority_inputs">Based on 5 inputs</div>
+          {priorityItems}
+          <div className="priority_wrapper_button">
+            <Button
+              size="small"
+              color="purple"
+              text="Weigh In"
+              onClick={() => setOpenMyVote(true)}
+            />
+          </div>
+          <div className="priority_wrapper_footer">
+            <Icon size="small" name="priority_4d4d4d.svg" />
+            Locate this card on priority view mode
+          </div>
         </div>
-        <div className="priority_wrapper_footer">
-          <Icon size="small" name='priority_4d4d4d.svg' />
-          Locate this card on priority view mode
+      )}
+
+      {openMyVote && (
+        <div className="my_vote">
+          {priorityItems}
+          <div className="priority_wrapper_button">
+            <Button
+              size="small"
+              color="purple"
+              text="Remove My Input"
+              onClick={() => setOpenMyVote(false)}
+            />
+          </div>
+          <div className="my_vote_info priority_wrapper_footer">
+            Last modified Aug 23, 2019 12:33pm
+          </div>
         </div>
-      </div>}
-
-      {openMyVote && <div className="my_vote">
-        {priorityItems}
-        <div className="priority_wrapper_button">
-          <Button size="small" color="purple" text="Remove My Input" onClick={() => setOpenMyVote(false)} />
-        </div>
-        <div className="my_vote_info priority_wrapper_footer">Last modified Aug 23, 2019 12:33pm</div>
-      </div>}
-
-    </div >
-
-  )
+      )}
+    </div>
+  );
 }
 
 Priority.propTypes = {
-  onClose: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    whoami: state.whoami,
+    goalAddress: state.ui.goalForm.editAddress
+  };
+}
+/*const mapDispatchToProps=(dispatch)=> {
+  return {
+    createGoalVote: (Vote) => {
+      return dispatch(addVoteOfGoal.create(Vote))
+    }
+}
+const mapStateToProps=(state) =>{
+  return {
+    whoami:state.whoami
+  }
+}*/
+
+function mapDispatchToProps(dispatch) {
+  return {
+    createGoalVote: goal_vote => {
+      return dispatch(addVoteOfGoal.create(goal_vote));
+    },
+    updateGoalVote: (goal_vote, address) => {
+      return dispatch(updateGoalVote.create({ goal_vote, address }));
+    },
+    fetchGoalVotes: () => {
+      return dispatch(fetchGoalVotes.create({}));
+    }
+  };
 }
 
-export default Priority
+export default connect(mapStateToProps, mapDispatchToProps)(Priority);
