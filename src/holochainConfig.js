35bd37976094517aff0fda25bdcfa4bfadc9c4d7
Alex1812r <alex1812r@gmail.com>
2019-12-03 10:30:13 -0400

    added styles for textarea in expandedViewModeContent.js

--- src/holochainConfig.js
+++ src/holochainConfig.js
@@ -1,4 +1,4 @@
 export const DEVELOPMENT_INSTANCE_NAME = 'acorn'
 export const ZOME_NAME = 'holo_acorn'
 export const DEFAULT_HOLOCHAIN_PORT = '8888'
-export const DEFAULT_HOLOCHAIN_HOST = '0.0.0.0'
+export const DEFAULT_HOLOCHAIN_HOST = '192.168.1.63'
