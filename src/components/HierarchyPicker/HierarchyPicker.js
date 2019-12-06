508f5192d702b87110867fb27f6b3e35c3cf56f2
Connor Turland <connorturland@gmail.com>
2019-12-06 11:40:51 -0500

    add PickerTemplate and use it for pickers

--- src/components/HierarchyPicker/HierarchyPicker.js
+++ src/components/HierarchyPicker/HierarchyPicker.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './HierarchyPicker.css'
-import Icon from '../Icon/Icon'
+import PickerTemplate from '../PickerTemplate/PickerTemplate'
 import { connect } from 'react-redux'
 
 function HierarchyPicker({ selectedHierarchy, hierarchyClicked, onClose }) {
@@ -32,16 +32,10 @@ function HierarchyPicker({ selectedHierarchy, hierarchyClicked, onClose }) {
   ]
 
   return (
-    <div className='hierarchies_picker vertical_action_overlay'>
-      <div className='hierarchy_picker_header'>
-        <span className='hierarchies_picker_title'>Hierarchy</span>
-        <Icon
-          className='vertical_action_close'
-          name='x_a3a3a3.svg'
-          size='small-close'
-          onClick={() => onClose()}
-        />
-      </div>
+    <PickerTemplate
+      className='hierarchies_picker'
+      heading='Hierarchy'
+      onClose={onClose}>
       <div className='hierarchy_content_wrapper'>
         {hierarchies.map((hierarchy, index) => (
           <HierarchyOption
@@ -53,7 +47,6 @@ function HierarchyPicker({ selectedHierarchy, hierarchyClicked, onClose }) {
             onClick={hierarchy => hierarchyClicked(hierarchy)}
           />
         ))}
-
         <p className='gray-text'>
           Not sure how to set the hierarchy for this card? Read more on our{' '}
           <a className='gray-text' href='#'>
@@ -61,7 +54,7 @@ function HierarchyPicker({ selectedHierarchy, hierarchyClicked, onClose }) {
           </a>
         </p>
       </div>
-    </div>
+    </PickerTemplate>
   )
 }
 
