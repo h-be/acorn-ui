7d4a861dc8dcd30592b97d340500ce419f68029b
Kevin Blanco <kevjoseph.66@gmail.com>
2019-12-16 16:00:42 -0400

    Co-authored-by: Connor Turland <connorturland@gmail.com>
    changes from Kevin Blanco

---
+++ src/components/ExpandedViewMode/ExpandedViewModeContent/activity history/ActivityHistory.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ActivityHistory = () => {
+  return <h1>Historial</h1>
+}
+
+export default ActivityHistory
---
+++ src/components/ExpandedViewMode/ExpandedViewModeContent/comments/Comments.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Comments = () => {
+  return <h1>Comentarios</h1>
+}
+
+export default Comments
