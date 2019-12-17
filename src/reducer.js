0b15bc08b5dd991b1e68edee4b01de0f0ba4de99
hector vasquez <hectorvasquez05@gmail.com>
2019-12-17 10:59:20 -0400

    change name folder

--- src/reducer.js
+++ src/reducer.js
@@ -24,7 +24,7 @@ import viewport from './viewport/reducer'
 import expandedView from './expanded-view/reducer'
 import goalVotes from './goal-votes/reducer'
 import goalComments from './goal-comments/reducer'
-import goalClone from './goal_clone/reducer'
+import goalClone from './goal-clone/reducer'
 
 // import anotherone from './another/path'
 
