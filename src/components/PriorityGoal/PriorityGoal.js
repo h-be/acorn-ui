4bc111dedc0fc6291dfdafa97794119cea801a77
Pegah <47722137+pegahv@users.noreply.github.com>
2019-12-19 17:09:46 -0500

    styling improvements and fixes for IndetedTreeView, PriorityQuadrant and Uncategorized tab

--- src/components/PriorityGoal/PriorityGoal.js
+++ src/components/PriorityGoal/PriorityGoal.js
@@ -12,33 +12,51 @@ function PriorityGoal({ goal }) {
     : null
   const toDate = goal.time_frame ? moment.unix(goal.time_frame.to_date) : null
 
+  // you can use these as values for
+  // testing/ development, instead of `squirrels`
+  // const testSquirrels = [
+  //  { avatar_url: 'img/profile.png' },
+  //  { avatar_url: 'img/profile.png' },
+  //  { avatar_url: 'img/profile.png' },
+  //]
+
   return (
     <div className='priority-quadrant-goal-item'>
-      <div className='priority-quadrant-goal-title'>
+      <div className='priority-quadrant-goal-icon'>
         <Icon name='leaf_incomplete.svg' size='small' />
-        {goal.content}
       </div>
-      <div className='priority-quadrant-goal-info'>
-        {goal.time_frame && (
-          <>
-            <Icon name='calendar_898989.svg' size='very-small' />
-            {fromDate && fromDate.format('MMM D, YYYY')}
-            {toDate && ' - '}
-            {toDate && toDate.format('MMM D, YYYY')}
-          </>
-        )}
-        {goal.members.length
-          ? goal.members.map((goalMember, index) => (
-              <Avatar key={index} avatar_url={goalMember.avatar_url} small />
-            ))
-          : null}
-      </div>
-      <div className='priority-quadrant-goal-right-menu'>
-        <div className='priority-quadrant-goal-view-mode-icons'>
-          <NavLink to='/board/map'>
-            <Icon name='map_aaaaa.svg' size='view-mode-small' />
-          </NavLink>
-          <Icon name='timeline_aaaaa.svg' size='view-mode-small' />
+      <div className='priority-quadrant-goal-content'>
+        <div className='priority-quadrant-goal-titleANDinfo'>
+          <div className='priority-quadrant-goal-title'>{goal.content}</div>
+          <div className='priority-quadrant-goal-info'>
+              {goal.time_frame && (
+                <div className='priority-quadrant-goal-timeframe'>
+                  <Icon name='calendar_898989.svg' size='very-small' className='grey not-hoverable' />
+                  {fromDate && fromDate.format('MMM D, YYYY')}
+                  {toDate && ' - '}
+                  {toDate && toDate.format('MMM D, YYYY')}
+                </div>
+              )}
+            <div className='priority-quadrant-goal-squirrels'>
+              {goal.members
+                ? goal.members.map((goalMember, index) => (
+                    <Avatar
+                      key={index}
+                      avatar_url={goalMember.avatar_url}
+                      small
+                    />
+                  ))
+                : null}
+            </div>
+          </div>
+        </div>
+        <div className='priority-quadrant-goal-right-menu'>
+          <div className='priority-quadrant-goal-view-mode-icons'>
+            <NavLink to='/board/map'>
+              <Icon name='map_aaaaa.svg' size='view-mode-small' className='grey' />
+            </NavLink>
+            <Icon name='timeline_aaaaa.svg' size='view-mode-small' className='grey' />
+          </div>
         </div>
       </div>
     </div>
