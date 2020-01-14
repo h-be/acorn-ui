602c2e6505f7c53b4de373cecc2f8fec2779e588
Connor Turland <connorturland@gmail.com>
2020-01-14 12:28:39 -0800

    code review changes

--- src/drawing/layoutFormula.js
+++ src/drawing/layoutFormula.js
@@ -7,30 +7,34 @@ export default function layoutFormula(screenWidth, goals, edges) {
   const goalsAsArray = goalsAddressesArray.map(address => goals[address])
   const edgeAddressesArray = Object.keys(edges)
   const edgesAsArray = edgeAddressesArray.map(address => edges[address])
+  // create a graph
+  const graph = new dagre.graphlib.Graph()
+    .setGraph({})
+    .setDefaultEdgeLabel(function() {
+      return {}
+    })
+  // add each Goal as a node in the graph
+  goalsAsArray.forEach(goal => {
+    graph.setNode(goal.address, {
+      width: goalWidth,
+      height: goalHeight,
+    })
+  })
+  // add each edge as an edge in the graph
+  edgesAsArray.forEach(edge => {
+    graph.setEdge(edge.parent_address, edge.child_address)
+  })
+  // run the layout algorithm, which will set an x and y property onto
+  // each node
+  dagre.layout(graph)
+  // create a coordinates object
   const coordinates = {}
-  if (goalsAsArray.length) {
-    const g = new dagre.graphlib.Graph()
-      .setGraph({})
-      .setDefaultEdgeLabel(function() {
-        return {}
-      })
-
-    goalsAsArray.forEach(goal => {
-      g.setNode(goal.address, {
-        width: goalWidth,
-        height: goalHeight,
-      })
-    })
-    edgesAsArray.forEach(edge => {
-      g.setEdge(edge.parent_address, edge.child_address)
-    })
-    dagre.layout(g)
-    g.nodes().forEach(v => {
-      coordinates[v] = {
-        x: g.node(v).x,
-        y: g.node(v).y,
-      }
-    })
-  }
+  // update the coordinates object
+  graph.nodes().forEach(address => {
+    coordinates[address] = {
+      x: graph.node(address).x,
+      y: graph.node(address).y,
+    }
+  })
   return coordinates
 }
