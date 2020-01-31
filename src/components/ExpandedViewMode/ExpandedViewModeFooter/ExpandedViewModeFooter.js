8801bd2a42898ce9c57b78a72f6c5db7abf03714
Connor Turland <connorturland@gmail.com>
2020-01-30 22:10:53 -0800

    touch up with TODO comments

--- src/components/ExpandedViewMode/ExpandedViewModeFooter/ExpandedViewModeFooter.js
+++ src/components/ExpandedViewMode/ExpandedViewModeFooter/ExpandedViewModeFooter.js
@@ -8,6 +8,7 @@ export default function ExpandedViewModeFooter({ goal, creator }) {
   return (
     <div className='expanded_view_footer'>
       <div className='footer_children_info'>
+        {/* TODO make this a dynamically calculated value */}
         <div className='footer_leaf_completes feature-in-development'>
           <Icon
             name='leaf_complete.svg'
@@ -16,6 +17,7 @@ export default function ExpandedViewModeFooter({ goal, creator }) {
           />{' '}
           81/164
         </div>
+        {/* TODO make this a dynamically calculated value */}
         <div className='footer_status_unknowns feature-in-development'>
           <Icon
             name='status_unknown.svg'
