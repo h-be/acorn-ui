3f37c9d645d4fd5b2664072d45f1fe4d5c2cc05f
PegahV <53795528+pegahvaezi@users.noreply.github.com>
2020-02-10 18:15:24 -0500

    added export guide + added tooltip options for icons on hover + improved display of profile form on small screens + a few other styling improvements

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,24 +5,35 @@ import './Footer.css'
 import Icon from '../Icon/Icon'
 
 function Footer() {
-
-  const mapPage = useRouteMatch("/board/map")
+  const mapPage = useRouteMatch('/board/map')
 
   let bottomRightPanelClassName = 'bottom-right-panel'
 
-  bottomRightPanelClassName = bottomRightPanelClassName + (mapPage ? '' : ' bottom-right-panel-not-map')
-
+  bottomRightPanelClassName =
+    bottomRightPanelClassName + (mapPage ? '' : ' bottom-right-panel-not-map')
 
   return (
     <div className='footer'>
-      <div className={bottomRightPanelClassName} >
+      <div className={bottomRightPanelClassName}>
         {mapPage && <Zoom />}
         <div className='view-mode-icons'>
           <NavLink to='/board/map' activeClassName='view-mode-active'>
-            <Icon name='map.svg' size='view-mode' className='grey' />
+            <Icon
+              name='map.svg'
+              size='view-mode'
+              className='grey'
+              withTooltipTop
+              tooltipText='map view'
+            />
           </NavLink>
           <NavLink to='/board/priority' activeClassName='view-mode-active'>
-            <Icon name='priority_898989.svg' size='view-mode' className='grey' />
+            <Icon
+              name='priority_898989.svg'
+              size='view-mode'
+              className='grey'
+              withTooltipTop
+              tooltipText='priority view'
+            />
           </NavLink>
           {/* <Icon name='timeline_898989.svg' className='grey' size='view-mode' /> */}
         </div>
