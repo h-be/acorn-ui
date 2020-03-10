5d79b504461375386b5cb8e3459e320051d153cf
Pegah <47722137+pegahv@users.noreply.github.com>
2020-03-10 12:43:44 -0400

    create dashbaord js and css + create scratchpad.js

---
+++ src/scratchpad.js
@@ -0,0 +1,19 @@
+
+
+
+/* TO Create a new page... 
+
+1. create a new folder within /src/routes with the component js and css in it, with the component style naming (caps)
+
+2. Copy in boilerplate react component code from another component, or use the 'component' shortcut in VSCode
+
+3. Open up /src/routes/App.js
+
+4. Import the new component
+
+5. Add a route within the 'Switch' component 
+
+6. Go to the path in the browser
+
+
+*/
