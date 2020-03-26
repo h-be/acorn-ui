71263951cf90ef191f2060574ebc99aa7d335815
Connor Turland <connorturland@gmail.com>
2020-03-26 16:09:08 -0400

    fix copy-paste for project secrets

--- src/components/InviteMembersModal/InviteMembersModal.js
+++ src/components/InviteMembersModal/InviteMembersModal.js
@@ -1,25 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './InviteMembersModal.css'
 
-import Icon from '../Icon/Icon'
-import Button from '../Button/Button'
-import Avatar from '../Avatar/Avatar'
-import ValidatingFormInput from '../ValidatingFormInput/ValidatingFormInput'
 import Modal from '../Modal/Modal'
 import {
   ProjectModalButton,
   ProjectModalContent,
   ProjectModalContentSpacer,
   ProjectModalHeading,
-  ProjectModalSubHeading,
 } from '../ProjectModal/ProjectModal'
+import ProjectSecret from '../ProjectSecret/ProjectSecret'
 
 export default function InviteMembersModal({ showModal, onClose, passphrase }) {
-  const reset = () => {
-    setProjectSecret('')
-    setValidatingSecret(false)
-  }
-
   const onDone = () => {
     onClose()
   }
@@ -33,18 +24,7 @@ export default function InviteMembersModal({ showModal, onClose, passphrase }) {
       <ProjectModalHeading title='Invite members to project' />
       <ProjectModalContent>
         <ProjectModalContentSpacer>
-          <div className='project-secret-row'>
-            <ValidatingFormInput
-              value={passphrase}
-              label='Project invitation secret'
-              helpText='Share this secret phrase with people you want to invite to this project'
-            />
-            {/*  TODO: add onclick listener */}
-            <div>
-              <Icon name='copy.svg' />
-              copy
-            </div>
-          </div>
+          <ProjectSecret passphrase={passphrase} />
         </ProjectModalContentSpacer>
       </ProjectModalContent>
       <ProjectModalButton text='Done' onClick={onDone} />
