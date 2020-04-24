a8c8afdf75eb9b7411d95c5641e0460219b797b0
Connor Turland <connorturland@gmail.com>
2020-04-24 16:36:01 -0400

    fix build command

--- nix/default.nix
+++ nix/default.nix
@@ -10,7 +10,7 @@
  ''
  set -euxo pipefail
  curl -O -L https://github.com/h-be/acorn-hc/releases/download/v''${1:-0.3.2}/projects.dna.json
- export PROJECTS_DNA_ADDRESS="$(hc hash --path projects.dna.json | awk '/DNA Hash: /{print $NF}' | tr -d '\n')"
+ export PROJECTS_DNA_ADDRESS="'$(hc hash --path projects.dna.json | awk '/DNA Hash: /{print $NF}' | tr -d '\n')'"
  ${holonix.pkgs.nodejs}/bin/npm run build
  cd dist && zip acorn-ui -r . && cd ..
  '')
