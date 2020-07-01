4a1881a6af6535a68aa8670570a54f88f83bd74b
Connor Turland <connorturland@gmail.com>
2020-07-01 11:35:58 -0400

    reconfig nix for releasing

--- default.nix
+++ default.nix
@@ -38,6 +38,7 @@ with holonix.pkgs;
    ++ config.buildInputs
    ++ (holonix.pkgs.callPackage ./nix {
     holonix = holonix;
+    config = config;
    }).buildInputs
   ;
  });
--- nix/default.nix
+++ nix/default.nix
@@ -1,18 +1,7 @@
-{ holonix }:
+{ holonix, config }:
 {
- buildInputs = [
- (holonix.pkgs.writeShellScriptBin "acorn-ui"
- ''
- set -euxo pipefail
- ${holonix.pkgs.nodejs}/bin/npm start
- '')
- (holonix.pkgs.writeShellScriptBin "acorn-build"
- ''
- set -euxo pipefail
- curl -O -L https://github.com/h-be/acorn-hc/releases/download/v''${1:-0.3.2}/projects.dna.json
- export PROJECTS_DNA_ADDRESS="'$(hc hash --path projects.dna.json | awk '/DNA Hash: /{print $NF}' | tr -d '\n')'"
- ${holonix.pkgs.nodejs}/bin/npm run build
- cd dist && zip acorn-ui -r . && cd ..
- '')
- ];
+ buildInputs = []
+ ++ ( holonix.pkgs.callPackage ./development { pkgs = holonix.pkgs; }).buildInputs
+ ++ ( holonix.pkgs.callPackage ./release { pkgs = holonix.pkgs; config = config; }).buildInputs
+ ;
 }
