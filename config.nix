44755e87e353ac018da6b573b0fbef91f5e9e04a
Connor Turland <connorturland@gmail.com>
2020-07-28 13:44:22 -0400

    v0.3.8

--- config.nix
+++ config.nix
@@ -67,8 +67,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.3.7";
-   previous = "0.3.6";
+   current = "0.3.8";
+   previous = "0.3.7";
   };
 
   github = {
