66daf8f6fd55d45bbec14918eef36cf774521ada
Connor Turland <connorturland@gmail.com>
2020-07-28 13:28:28 -0400

    drop error thrower mistakenly committed

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './LoadingScreen.css'
 
 function LoadingScreen() {
-  throw new Error('eh')
   return (
     <div className='loading_screen_wrapper'>
       <div className='loading_screen'>
